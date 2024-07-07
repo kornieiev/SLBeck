cc3dea2bdcec0f582e4c9a96a9469581d464733c
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-07 14:32:04 +0200

    fix route getKeysByMaker

--- controllers/keys/getKeysByMaker.js
+++ controllers/keys/getKeysByMaker.js
@@ -3,6 +3,7 @@ const { Key } = require("../../models");
 const getKeysByMaker = async (req, res, next) => {
   const { _id: owner } = req.user;
   const { maker } = req.body;
+  console.log("🚀 ~ getKeysByMaker ~ req.body:", req.body);
   console.log("🚀 ~ getKeysByMaker ~ maker:", maker);
 
   const { page = 1, limit = "" } = req.query; // важливо вказати значення за замовчуванням
@@ -10,10 +11,14 @@ const getKeysByMaker = async (req, res, next) => {
   const skip = (page - 1) * limit;
 
   if (req.user.role === "admin") {
-    const result = await Key.find({ Maker: maker }, "-createdAt -updatedAt", {
-      skip,
-      limit,
-    }).populate("owner", "name email"); // назва поля, яке потрібно поширити. Візьми поле "owner" знайди з якої колекції воно і пошир дані в цієї колекції замість поля "owner"
+    const result = await Key.find(
+      { Maker: `${maker}` },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "name email"); // назва поля, яке потрібно поширити. Візьми поле "owner" знайди з якої колекції воно і пошир дані в цієї колекції замість поля "owner"
     // другим аргументом вказується список полей, які потрібно повернути
     res.status(200).json(result);
     return;
