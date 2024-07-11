24bccbf318a0e7135e7665da922935f775cfdc36
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-11 12:14:31 +0200

    clg to updateById

--- controllers/keys/updateById.js
+++ controllers/keys/updateById.js
@@ -3,8 +3,13 @@ const { Key } = require("../../models");
 
 const updateById = async (req, res, next) => {
   const { id } = req.params;
+  console.log("req.params ~ id:", id);
+  const data = req.body;
+  console.log("req.body ~ data:", data);
+
   const result = await Key.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
+    ``;
     throw HttpError(404);
   }
   res.status(200).json(result);
