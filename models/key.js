2db12cbf20fc061e02a5e52982a1a497a5357f21
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-15 23:05:29 +0200

    fix: keysSchema - change required fields

--- models/key.js
+++ models/key.js
@@ -31,54 +31,77 @@ const keysSchema = new Schema(
     },
     "Price All Keys Lost": {
       type: String,
-      // required: [true, "Set PRICE ALL KEYS LOST"],
+      required: [false],
     },
     "Price Add a Key": {
       type: String,
+      required: [false],
       // required: [true, "Set PRICE ADD KEY"],
     },
     "Price Program Only": {
       type: String,
+      required: [false],
+
       // required: [true, "Set PRICE PROGRAM ONLY"],
     },
     "Dealer Price": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER PRICE"],
     },
     "Dealer Program": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER PROGRAM"],
     },
     "Dealer Emergency Blade": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER EMERGENCY BLADE"],
     },
     "Dealer Price Total": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER TOTAL PRICE"],
     },
     "Dealer Location": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER LOCATION"],
     },
     "Secure Locks Parts": {
       type: String,
+      required: [false],
+
       // required: [true, "Set DEALER LOCKS PART"],
     },
     "Part #": {
       type: String,
+      required: [false],
+
       // required: [true, "Set PART NUM"],
     },
     Link: {
       type: String,
+      required: [false],
+
       // required: [true, "Set LINK"],
     },
     Comments: {
       type: String,
+      required: [false],
+
       // required: [true, "Set COMMENT"],
     },
     isActive: {
       type: Boolean,
+      required: [false],
+
       // required: [true, "Set isActive"],
     },
     owner: {
