cfe264aba413df4cd531e18223dd27fb47562b87
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-15 14:46:27 +0200

    features: createKey route

--- controllers/keys/createKey.js
+++ controllers/keys/createKey.js
@@ -1,9 +1,14 @@
 const { Key } = require("../../models");
 
 const createKey = async (req, res, next) => {
-  const { _id: owner } = req.user;
+  // const { _id: owner } = req.user;
 
-  const result = await Key.create({ ...req.body, owner });
+  console.log("createKey ...req.body:", ...req.body);
+
+  // const result = await Key.create({ ...req.body, owner });
+  const result = await Key.create({ ...req.body });
+  console.log("🚀 ~ createKey ~ result:", result);
+
   res.status(201).json({ result });
 };
 
