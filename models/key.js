72b508cb367aa0370908ea806292d2a745e32f48
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-07-16 12:57:50 +0200

    fix: createKeySchema

--- models/key.js
+++ models/key.js
@@ -21,88 +21,52 @@ const keysSchema = new Schema(
     },
     "Type of Key": {
       type: String,
-      // required: [true, "Set TYPE OF KEY"],
+      required: [true, "Set TYPE OF KEY"],
     },
     "No Buttons": {
       type: Number,
       min: [1, "No Buttons must be at least 1"],
       max: [10, "No Buttons must be at most 10"],
-      // required: [true, "Set NUMBER OF BUTTONS"],
+      required: [true, "Set NUMBER OF BUTTONS"],
     },
     "Price All Keys Lost": {
       type: String,
-      // required: [false],
     },
     "Price Add a Key": {
       type: String,
-      // required: [false],
-      // required: [true, "Set PRICE ADD KEY"],
     },
     "Price Program Only": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set PRICE PROGRAM ONLY"],
     },
     "Dealer Price": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER PRICE"],
     },
     "Dealer Program": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER PROGRAM"],
     },
     "Dealer Emergency Blade": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER EMERGENCY BLADE"],
     },
     "Dealer Price Total": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER TOTAL PRICE"],
     },
     "Dealer Location": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER LOCATION"],
     },
     "Secure Locks Parts": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set DEALER LOCKS PART"],
     },
     "Part #": {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set PART NUM"],
     },
     Link: {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set LINK"],
     },
     Comments: {
       type: String,
-      // required: [false],
-
-      // required: [true, "Set COMMENT"],
     },
     isActive: {
       type: Boolean,
-      // required: [false],
-
-      // required: [true, "Set isActive"],
     },
     owner: {
       type: Schema.Types.ObjectId, // вказуємо зберігати id
